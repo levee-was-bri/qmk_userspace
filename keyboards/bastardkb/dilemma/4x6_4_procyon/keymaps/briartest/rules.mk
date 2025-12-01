607a3560d99115b46398d5da30c4985f326b2877
briar <briarharvey@gmail.com>
2025-12-01 14:41:15 -0500

    make ai suggested changes

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/briartest/rules.mk
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/briartest/rules.mk
@@ -2,3 +2,5 @@ VIA_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 COMBO_ENABLE = yes
 MOUSEKEY_ENABLE = yes
+SPLIT_KEYBOARD = yes
+USE_I2C = no  # Start with serial; switch to I2C if your wiring uses SDA/SCL
